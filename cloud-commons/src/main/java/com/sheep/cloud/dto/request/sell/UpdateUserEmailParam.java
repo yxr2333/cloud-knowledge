d3df9ff8c4678e874c6bba8e608cb6ee046b0316
Zhang Jinming <553781523@qq.com>
2022-11-26 19:36:23 +0800

    userinfo的增删改操作

--- cloud-commons/src/main/java/com/sheep/cloud/dto/request/sell/UpdateUserEmailParam.java
+++ cloud-commons/src/main/java/com/sheep/cloud/dto/request/sell/UpdateUserEmailParam.java
@@ -1,10 +1,34 @@
 package com.sheep.cloud.dto.request.sell;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 /**
  * @author Zhang Jinming
  * @date 25/11/2022 下午5:18
  */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateUserEmailParam {
+    @NotNull(message = "用户编号不能为空")
     private Integer id;
+
+    @NotNull(message = "未选择填写邮箱")
+    @Pattern(regexp = "^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$",
+            message = "邮箱格式不正确")
     private String email;
+
+    @NotNull(message = "未填写验证码")
+    private String code;
+
+    @Length(min = 10, max = 10, message = "请求标识符长度必须为10")
+    private String requestKey;
 }
--- cloud-sell/src/main/java/com/sheep/cloud/service/impl/IUserInfoServiceImpl.java
+++ cloud-sell/src/main/java/com/sheep/cloud/service/impl/IUserInfoServiceImpl.java
@@ -1,13 +1,17 @@
 package com.sheep.cloud.service.impl;
 
 import com.sheep.cloud.dao.sell.ISellUserEntityRepository;
+import com.sheep.cloud.dto.request.sell.UpdateUserEmailParam;
+import com.sheep.cloud.dto.request.sell.UpdateUserInfoParam;
 import com.sheep.cloud.dto.response.ApiResult;
 import com.sheep.cloud.entity.sell.ISellUserEntity;
 import com.sheep.cloud.entity.sell.ISellUserInfoEntity;
 import com.sheep.cloud.service.IUserInfoService;
+import com.sheep.cloud.store.RedisUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Zhang Jinming
@@ -21,14 +25,39 @@ public class IUserInfoServiceImpl implements IUserInfoService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private RedisUtil redisUtil;
+
     @Override
-    public ApiResult<?> publishOne() {
-        return null;
+    @Transactional(rollbackFor = Exception.class)
+    public ApiResult<?> updateEmail(UpdateUserEmailParam emailParam) {
+        String requestKey = emailParam.getRequestKey();
+        Object o = redisUtil.get(requestKey);
+        if (o == null) {
+            return new ApiResult<>().error("未获取验证码或验证码已过期");
+        }
+        String code = o.toString();
+        if (code.equals(emailParam.getCode())) {
+            ISellUserEntity userEntity = userEntityRepository.findById(emailParam.getId())
+                    .orElseThrow(() -> new RuntimeException("未查询到用户信息"));
+            userEntity.setEmail(emailParam.getEmail());
+            userEntityRepository.save(userEntity);
+            redisUtil.delete(requestKey);
+            return new ApiResult<>().success("邮箱更新成功！");
+        }
+        return new ApiResult<>().error("验证码不正确！");
     }
 
     @Override
-    public ApiResult<?> updateOne() {
-        return null;
+    @Transactional(rollbackFor = Exception.class)
+    public ApiResult<?> updateBasicInfo(UpdateUserInfoParam userInfo) {
+        ISellUserEntity userEntity = userEntityRepository.findById(userInfo.getId())
+                .orElseThrow(() -> new RuntimeException("未查询到用户信息"));
+        userEntity.setUsername(userInfo.getUsername());
+        userEntity.setDescription(userInfo.getDescription());
+        userEntity.setAvatar(userInfo.getAvatar());
+        userEntityRepository.save(userEntity);
+        return new ApiResult<>().success("信息更新成功！");
     }
 
     @Override
